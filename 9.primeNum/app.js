d54f30cd691420916b690e31a48a0ffdf7bef058
Juubu-hub <malikju7357@gmail.com>
2025-04-11 19:38:12 +0530

    Update app.js

--- 9.primeNum/app.js
+++ 9.primeNum/app.js
@@ -1,6 +1,6 @@
 let num = parseInt(prompt("enter any number"));
 function prime(n) {
-  if (n / n && n / 1 && !(n % 2 == 0)) {
+  if (n / n && n / 1 && !(n % 2 == 0)||n==2) {
     console.log("num is prime");
   } else {
     console.log("num is not prime ");
